8ea54751d6df18758f770e25425628af4268fa09
lily <lily.you@student.tdsb.on.ca>
2025-03-25 22:41:34 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,15 @@ public class PracticeProblem {
 
 	}
 
-
+	public static void insertionSort(char[] characters){
+		for(int i = 0; i < characters.length; i++){
+			char key = characters[i];
+			int index = i - 1;
+			while(index >= 0 && key < characters[index]){
+				characters[index + 1] = characters[index];
+				index--;
+			}
+			characters[index + 1] = key;
+		}
+	}
 }
